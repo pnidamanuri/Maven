2241c7bb5eba722e1c4b72bfa99233491f0596fc
pnidamanuri <58285431+pnidamanuri@users.noreply.github.com>
2019-12-05 13:54:22 +0530

    2

---
+++ maven/reports/html/report.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/main/java/com/maven/stepdefinitions/login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Flipkart application test",
+  "description": "",
+  "id": "flipkart-application-test",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "comments": [
+    {
+      "line": 3,
+      "value": "#Background:"
+    }
+  ],
+  "line": 5,
+  "name": "Login to Flipkart",
+  "description": "",
+  "id": "flipkart-application-test;login-to-flipkart",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 7,
+  "name": "user on home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "Login to the application",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I go to Iphone store",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "I navigate to Samsung store and select Sansung J8",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I quit broswer",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Login_step_definition.user_on_home_page()"
+});
+formatter.result({
+  "duration": 7528906319,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login_step_definition.login_to_the_application()"
+});
+formatter.result({
+  "duration": 440348287,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login_step_definition.i_go_to_iphone_store()"
+});
+formatter.result({
+  "duration": 2775808842,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login_step_definition.i_navigate_to_samsung_store_and_select_sansung_j8()"
+});
+formatter.result({
+  "duration": 4636476762,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Login_step_definition.i_quit_broswer()"
+});
+formatter.result({
+  "duration": 1246640528,
+  "status": "passed"
+});
+});
